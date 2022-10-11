bf0509c7ac66addedfb5c811cedcf98effb8d0d8
Rod76m <rodrigue.mrt@gmail.com>
2022-10-11 14:42:08 +0200

    Update dataController.js

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -2,21 +2,19 @@ const Data = require('../data/stupid.json');
 
 exports.index = (req, res) => {
   res.json(Data)
-  
 };
 
 exports.insert = (req, res) =>{
  res.json(Data.people.push({name:'Rodrigue', age:20, address:{streetAddress:"test",city:"rouen",postalCode:"76000",state:'test'},phoneNumber:"5"}))
-}
+};
 
 exports.delete = (req, res) => {
-  res.json(Data.people)
-}
+  res.json(Data.people.pop())
+};
 
 exports.getUsersName = (req, res) => {
-    res.json(Data.people.map((i, index) => {
-      return Data.people[index].name
-    }) )
-
-  
-}
+  res.json(Data.people.map((i, index) => {
+    return Data.people[index].name
+  })
+  )  
+};
