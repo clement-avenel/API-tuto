28df5ea7031c9f22d0105f3e95067a7f6a209f85
YorimNNN <yorim.niabe@campus-la-chataigneraie.org>
2022-10-11 14:31:30 +0200

    get people number

--- api-routes.js
+++ api-routes.js
@@ -3,9 +3,8 @@ const router = require('express').Router();
 
 // Import controllers
 const dataController = require('./controllers/dataController');
-
 // Data routes
 router.get('/', dataController.index);
-
+router.get('/people_number', dataController.people_number)
 // Export API routes
 module.exports = router;
--- controllers/dataController.js
+++ controllers/dataController.js
@@ -3,3 +3,8 @@ const Data = require('../data/stupid.json');
 exports.index = (req, res) => {
   res.json(Data)
 };
+
+exports.people_number= (req, res) => {
+  myPeople =Data.people[0];
+  res.json(myPeople['phoneNumber'][0]['number']);
+};
