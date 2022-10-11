5d1dedb2e6506296f0625ab987b02b07a57e3534
bastien-lch <74551941+bastien-lch@users.noreply.github.com>
2022-10-11 15:11:52 +0200

    mise a jour fonction getAddress
    Mise a jour de la reponse

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -10,5 +10,5 @@ exports.getAdresse = (req, res) => {
   Data['people'].forEach(element => {
     arrayAddress.push(element['address']);
   });
-  res.json(arrayAddress);
+  res.json({'adresse':arrayAddress});
 };
