83a3b6718a6ccfd3bce024a6f1d3962a6bd7f8f2
Jules-Latapy <jullatapy@gmail.com>
2022-10-11 14:08:05 +0200

    update : util function for phone number

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,5 +1,19 @@
 const Data = require('../data/stupid.json');
 
+exports.getPhoneNumbers = (req, res) => {
+    
+    var result = [];
+
+    for (var peoples of Data.people)
+
+        for (var phoneNumbers of peoples.phoneNumber)
+
+            result.push(phoneNumbers.number) ;
+
+    res.send(result);
+};
+
+
 exports.index = (req, res) => {
-  res.json(Data)
+    res.json(Data);
 };
