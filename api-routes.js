c1fa6669718f2f15a45ee163d10460a835771fb1
Kevin Laguel <laguel.kev@gmail.com>
2022-10-11 12:24:09 +0200

    feat: add method getOnlyUsersNumbers

--- api-routes.js
+++ api-routes.js
@@ -7,5 +7,8 @@ const dataController = require('./controllers/dataController');
 // Data routes
 router.get('/', dataController.index);
 
+// Only phones numbers
+router.get('/phones', dataController.getOnlyUsersNumbers);
+
 // Export API routes
 module.exports = router;
--- controllers/dataController.js
+++ controllers/dataController.js
@@ -3,3 +3,12 @@ const Data = require('../data/stupid.json');
 exports.index = (req, res) => {
   res.json(Data)
 };
+
+exports.getOnlyUsersNumbers = (req, res) => {
+  let phoneNumber = [];
+  let users = Data.people;
+  for (user of users){
+    phoneNumber.push(user.phoneNumber)
+  }
+  res.json(phoneNumber)
+};
