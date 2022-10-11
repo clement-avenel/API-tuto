f3730f66298ef91b3c5e9d4caa4347d0cfce8b0c
bastien-lch <74551941+bastien-lch@users.noreply.github.com>
2022-10-11 15:05:46 +0200

    ajout fonction getAddress
    Ajout d'une fonction pour afficher toute les adresses

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,5 +1,14 @@
+const req = require('express/lib/request');
 const Data = require('../data/stupid.json');
 
 exports.index = (req, res) => {
   res.json(Data)
+};
+
+exports.getAdresse = (req, res) => {
+  arrayAddress = [];
+  Data['people'].forEach(element => {
+    arrayAddress.push(element['address']);
+  });
+  res.json(arrayAddress);
 };
