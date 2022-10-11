63663b377aec6be184f852bfabda74ffffee2661
Pierre SCHELDEWAERT <pierre.scheldewaert@viacesi.fr>
2022-10-11 14:53:28 +0200

    add getPhoneNumbers route

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -3,3 +3,15 @@ const Data = require('../data/stupid.json');
 exports.index = (req, res) => {
   res.json(Data)
 };
+
+exports.getUserByPhoneNumber = (req, res) => {
+  const phoneNumbers = req.query.phone;
+  let users = [];
+
+  for (user of Data.people) {
+    users.push(user.phoneNumber);
+  }
+
+  res.json(users);
+
+};
